7ec6890eca0d68a76c47fa697020acbe0018d384
samirk98 <samwilliams661@gmail.com>
2023-05-18 13:10:28 +0400

    Intro-JS

---
+++ script.js
@@ -0,0 +1,84 @@
+// Primitive data types of JS
+// Number
+console.log(10);
+console.log(10.5);
+console.log(1e9); // 10 ^ 9
+console.log(NaN); // invalid number
+console.log(Infinity); // sonsuzluq
+console.log(-Infinity); // menfi sonsuzluq
+console.log(0); // zero
+console.log(-0); // negative zero
+
+
+// BigInt - big integer (ends with 'n')
+console.log(10n);
+
+
+//Boolean - (boolean algebra)
+console.log(true);
+console.log(false);
+
+
+// String - array of characters
+console.log('Pop smoke is best');
+console.log(`Pop Smoke got famous and died`);
+
+// undefined - nese var, amma deyeri yoxdu, not defined
+console.log(undefined);
+
+// null - objecting yoxlugun bildirir.
+console.log(null);
+
+// Symbol - her hansi bir tekrarlanmayan deyer
+console.log(Symbol('Test'));
+
+
+/**
+ * Variables and arithemtic operators
+ */
+
+// Decleration & Initzaliton
+
+// var currentYear = 2023;
+// var birthYear = 1998;
+
+// var age = currentYear - birthYear;
+
+// console.log(age);
+
+
+// x^2 - 3x - 4 = 0
+
+//  var a = 1; 
+//  var b = -3;
+//  var c = -4;
+
+//  var D = b**2 - 4 * a * c;
+
+//  var rootOfD = Math.sqrt(D);
+
+//  var x1 = (-b - rootOfD) / (2 * 1);
+//  var x2 = (-b + rootOfD) / (2 * 1);
+//  console.log(x1,x2);
+
+
+// let x = 5;
+
+// if(x < 6) {
+//     console.log(false);
+// } else {
+//     console.log(true);
+// }
+
+// let message = prompt("Enter your name ");
+
+// console.log("Hello " + message + " How are you?" );
+
+
+// let isConfirmed = confirm("Are you sure?");
+// console.log(isConfirmed);
+
+// type coercion
+// == loose equality
+// === strict equality
+
